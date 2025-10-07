eaba56b83827734c1dc95b93f27333588dcd1059
Chanakan393 <chana.sch17@gmail.com>
2025-10-07 20:34:15 +0700

    Add Basic Form to IndexPage.vue

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,13 +1,63 @@
 <template>
   <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
+    <q-form @submit.prevent="submitForm" @reset.prevent="resetForm" class="q-gutter-md">
+
+      <q-input
+        filled
+        label="Your name *"
+        v-model="form.name"
+      />
+
+      <q-input
+        filled
+        label="Your age *"
+        type="number"
+        v-model="form.age"
+      />
+
+      <q-checkbox
+        v-model="form.accept"
+        label="I accept the license and terms"
+      />
+
+      <div class="row q-gutter-sm">
+        <q-btn type="submit" label="Submit" color="primary"/>
+        <q-btn type="reset" label="Reset" color="secondary"/>
+      </div>
+
+    </q-form>
   </q-page>
 </template>
 
 <script setup>
-//
+import { ref } from 'vue'
+import { QForm, QInput, QCheckbox, QBtn, QPage } from 'quasar'
+
+const form = ref({
+  name: '',
+  age: '',
+  accept: false
+})
+
+function submitForm() {
+  if (!form.value.accept) {
+    alert('You need to accept the license and terms first')
+    return
+  }
+  if (!form.value.name) {
+    alert('Please type something for name')
+    return
+  }
+  if (form.value.age <= 0) {
+    alert('Please type a real age')
+    return
+  }
+  alert('Submitted')
+}
+
+function resetForm() {
+  form.value.name = ''
+  form.value.age = ''
+  form.value.accept = false
+}
 </script>
